ce3d77db56988e25d4b91c2c1fa56eb02b01c90b
Cristian Roman <cromanpa94@gmail.com>
2022-06-23 11:46:22 -0700

    Frequency

--- Analyses/0_preprocessing.R
+++ Analyses/0_preprocessing.R
@@ -1,7 +1,10 @@
+############################
+##### 0. Preprocessing #####
+############################
+
 library(here)
 library(pdftools)
 library(tidyverse)
-library(tidytext)
 library(tidytext)
 
 ## List files and keep them in a list
@@ -15,7 +18,7 @@ files.16.20 <- list('data16' = data16.files,
 text.16.20 <- lapply(files.16.20, function(y){
   map_df(y, ~ data_frame(txt = pdf_text(.x)) %>%
            mutate(filename = .x) %>%
-           unnest_tokens(word, txt))
+           unnest_tokens(text, txt))
 })
 
 
@@ -23,8 +26,30 @@ text.16.20 <- lapply(files.16.20, function(y){
 
 all(unlist(files.16.20) %in% unlist(lapply(text.16.20, `[`, 1)))
 
-##
+## Remove additional words and non-characters
+remove_reg <- "&amp;|&lt;|&gt;"
 
+text.16.20.c <- lapply(text.16.20, function(y){
+                  y %>% 
+                  mutate(text = str_remove_all(text, remove_reg)) %>%
+                  filter(!text %in% stop_words$word,
+                         !text %in% str_remove_all(stop_words$word, "'"),
+                         str_detect(text, "[a-z]"))
+})
 
+names(text.16.20.c) <- names(files.16.20)
+
+## Export pre-processed data
+
+lapply(seq_along(text.16.20.c), function(x){
+  write.csv(text.16.20.c[[x]], 
+            file = paste0(here("Data/preprocessed/"), 
+                          names(text.16.20.c)[x],
+                          '.csv'))
+})
+
+rm(list = ls()[!ls() %in% c("text.16.20.c")])
+
+save.image(here("Data/preprocessed/data_ws.RData"))
 
 
---
+++ Analyses/1_frequency.R
@@ -0,0 +1,65 @@
+############################
+####### 1. Frequency ######
+############################
+
+library(here)
+library(tidyverse)
+library(tidytext)
+library(data.table)
+library(scales)
+library(ggthemr)
+
+ggthemr("light")
+
+## Load workspace
+
+load(here("Data/preprocessed/data_ws.RData"))
+
+### Estimate frequency
+
+text.16.20.c.b <- rbindlist(text.16.20.c, idcol = 'year')
+
+text.16.20.c.f <- text.16.20.c.b %>% 
+                  count(year, text, sort = TRUE) %>% 
+                  left_join(text.16.20.c.b %>% 
+                              count(year, name = "total")) %>%
+                  mutate(freq = n/total)
+
+## Top X% of words per year
+threshold <- 0.1
+
+sub.16 <- text.16.20.c.f[text.16.20.c.f$year == 'data16',]
+sub.20 <- text.16.20.c.f[text.16.20.c.f$year == 'data20',]
+
+q.16 <- quantile(unlist(sub.16$freq), 1 - (threshold/100))
+q.20 <- quantile(unlist(sub.20$freq), 1 - (threshold/100))
+
+sub.16 <- sub.16[sub.16$freq >= q.16]
+sub.20 <- sub.20[sub.20$freq >= q.20]
+
+top.x.year <- rbind.data.frame(sub.16, sub.20)
+
+ggplot(data = top.x.year, aes(fill = year)) +
+  geom_bar(aes(x = text, y = freq), stat = "identity") +
+  facet_wrap(~year, ncol = 1, scales = 'free_y')
+  
+
+## Plot scatterplot frequencies
+
+text.16.20.c.f.t <- text.16.20.c.f %>% 
+  select(year, text, freq) %>% 
+  pivot_wider(names_from = year, values_from = freq) %>%
+  arrange(data16, data20)
+
+ggplot(text.16.20.c.f.t, aes(data16, data20)) +
+  geom_point(alpha = 0.1, size = 2.5, width = 0.25, height = 0.25) +
+  geom_text(aes(label = text), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  geom_abline(color = "red")
+
+summary(lm(text.16.20.c.f.t$data20 ~ text.16.20.c.f.t$data16))
+
+
+
+
